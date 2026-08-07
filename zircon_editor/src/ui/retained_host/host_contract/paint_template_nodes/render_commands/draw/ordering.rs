48cbdc54aa3709fea907a78a4d158ee04d38b3fc
He-Jiahui <814111601@qq.com>
2026-08-07 09:13:21 +0800

    perf(editor-ui): skip sorting ordered paint commands

--- zircon_editor/src/ui/retained_host/host_contract/paint_template_nodes/render_commands/draw/ordering.rs
+++ zircon_editor/src/ui/retained_host/host_contract/paint_template_nodes/render_commands/draw/ordering.rs
@@ -1,11 +1,19 @@
 use super::super::super::super::paint_frame::HostRgbaFrame;
 use super::super::command::HostPaintCommand;
 use super::dispatch::draw_host_paint_command;
+use crate::ui::retained_host::ui_perf::{record_current_ui_perf_counter, UiPerfCounter};
 
 pub(in crate::ui::retained_host::host_contract::paint_template_nodes) fn draw_host_paint_commands(
     frame: &mut HostRgbaFrame,
     commands: &[HostPaintCommand],
 ) -> bool {
+    if z_indices_are_ordered(commands.iter().map(|command| command.z_index)) {
+        return commands.iter().fold(false, |drew_any, command| {
+            draw_host_paint_command(frame, command) || drew_any
+        });
+    }
+
+    record_current_ui_perf_counter(UiPerfCounter::FallbackSortCount, 1.0);
     let mut ordered = {
         zircon_runtime::profile_scope!("editor", "host_painter", "paint_commands_collect_order");
         commands.iter().enumerate().collect::<Vec<_>>()
@@ -23,4 +31,31 @@ pub(in crate::ui::retained_host::host_contract::paint_template_nodes) fn draw_ho
         }
     }
     drew_any
+}
+
+fn z_indices_are_ordered(mut indices: impl Iterator<Item = i32>) -> bool {
+    let Some(mut previous) = indices.next() else {
+        return true;
+    };
+    indices.all(|current| {
+        let ordered = previous <= current;
+        previous = current;
+        ordered
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::z_indices_are_ordered;
+
+    #[test]
+    fn ordered_and_equal_layers_stay_on_the_zero_sort_path() {
+        assert!(z_indices_are_ordered([0, 0, 2, 8].into_iter()));
+        assert!(z_indices_are_ordered([].into_iter()));
+    }
+
+    #[test]
+    fn descending_layer_requires_the_fallback_sort() {
+        assert!(!z_indices_are_ordered([0, 4, 3, 8].into_iter()));
+    }
 }
